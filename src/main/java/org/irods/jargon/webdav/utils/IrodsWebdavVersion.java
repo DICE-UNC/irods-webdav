3429dd453b00209e6fb906ffff4c4514c6ea801f
test <test@irodsLinux.irods.org>
2015-05-21 13:16:05 -0400

    #9 added maven settings.xml activated swtich for build to milton-ce versus milton-enterprise

--- src/main/java/org/irods/jargon/webdav/utils/IrodsWebdavVersion.java
+++ src/main/java/org/irods/jargon/webdav/utils/IrodsWebdavVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.webdav.utils;
 public final class IrodsWebdavVersion {
  public static String VERSION="0.0.1-SNAPSHOT";
- public static String BUILD_TIME="2015-05-21T11:57:37Z";
+ public static String BUILD_TIME="20150521-1313";
 }
